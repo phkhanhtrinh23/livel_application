44d6cf362c2ac3b57c6c9ae83e083ec6b2ddea4c
Trinh Pham <57525136+phkhanhtrinh23@users.noreply.github.com>
2021-03-22 00:04:28 +0700

    fixed image animation not to rebuild

--- lib/home_screens/components/home.dart
+++ lib/home_screens/components/home.dart
@@ -1,79 +1,9 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:livel_application/home_screens/components/available_elements.dart';
 import 'package:livel_application/home_screens/components/countries_elements.dart';
+import 'package:livel_application/home_screens/components/events/events.dart';
 
-class EventElements extends StatelessWidget {
-  const EventElements({
-    Key key,
-    this.image,
-  }) : super(key: key);
-
-  final String image;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: 329,
-      height: 185,
-      margin: const EdgeInsets.only(
-        top: 8,
-        bottom: 40,
-      ),
-      alignment: Alignment.center,
-      decoration: BoxDecoration(
-        color: Color(0xFFFFEADE),
-        borderRadius: BorderRadius.circular(16),
-        image: DecorationImage(
-          image: AssetImage(image),
-          fit: BoxFit.fill,
-        ),
-      ),
-    );
-  }
-}
-
-class MainHome extends StatefulWidget {
-  MainHome({Key key}) : super(key: key);
-
-  @override
-  _MainHome createState() => _MainHome();
-}
-
-class _MainHome extends State<MainHome> with TickerProviderStateMixin {
-  List<EventElements> _events = [
-    EventElements(
-      image: 'images/events_1.png',
-    ),
-    EventElements(
-      image: 'images/events_2.png',
-    ),
-    EventElements(
-      image: 'images/events_3.png',
-    ),
-  ];
-  int _index = 0;
-  Timer _timer;
-  Duration _duration = Duration(seconds: 1);
-
-  @override
-  void initState() {
-    _timer = Timer.periodic(_duration, (Timer timer) {
-      setState(() {
-        _index = _index == _events.length - 1 ? 0 : _index + 1;
-      });
-    });
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    _timer.cancel();
-    _timer = null;
-    super.dispose();
-  }
-
+class MainHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var date = DateTime.now();
@@ -176,12 +106,12 @@ class _MainHome extends State<MainHome> with TickerProviderStateMixin {
             ),
           ),
         ),
-        AnimatedSwitcher(
-          duration: Duration(seconds: 1),
-          transitionBuilder: (Widget child, Animation<double> animation) {
-            return FadeTransition(child: child, opacity: animation);
-          },
-          child: _events[_index],
+        EventElements(
+          images: [
+            'images/events_1.png',
+            'images/events_2.png',
+            'images/events_3.png',
+          ],
         ),
         Row(
           children: <Widget>[

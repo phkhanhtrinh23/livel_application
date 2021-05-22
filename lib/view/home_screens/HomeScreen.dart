12ddcea6744805cc43aa998a6262254880a75289
Trinh Pham <57525136+phkhanhtrinh23@users.noreply.github.com>
2021-05-22 10:09:42 +0700

    Update Home Screen

--- lib/view/home_screens/HomeScreen.dart
+++ lib/view/home_screens/HomeScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:livel_application/view/home_screens/components/home.dart';
 import 'package:livel_application/view/home_screens/explore/explore.dart';
 import 'package:livel_application/view/home_screens/user_screen/user_main.dart';
@@ -22,40 +23,63 @@ class _HomeScreen extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: _widgetOptions.elementAt(_selectedIndex),
-      bottomNavigationBar: BottomAppBar(
-        child: BottomNavigationBar(
-          type: BottomNavigationBarType.fixed,
-          iconSize: 25,
-          showSelectedLabels: true,
-          showUnselectedLabels: true,
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'Home',
+    return WillPopScope(
+      onWillPop: () async => showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          content: Text('Are you sure you want to quit?'),
+          actions: <Widget>[
+            RaisedButton(
+              child: Text('Cancel'),
+              onPressed: () => Navigator.of(context).pop(false),
             ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.search),
-              label: 'Explore',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.list),
-              label: 'Your Trips',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.person),
-              label: 'User',
+            RaisedButton(
+              child: Text(
+                'Quit',
+                style: TextStyle(
+                  color: Color(0xFFEE6C4D),
+                ),
+              ),
+              onPressed: () => SystemNavigator.pop(),
             ),
           ],
-          backgroundColor: Color(0xFF4EAFC1),
-          currentIndex: _selectedIndex,
-          selectedItemColor: Colors.white,
-          onTap: (value) {
-            setState(() {
-              _selectedIndex = value;
-            });
-          },
+        ),
+      ),
+      child: Scaffold(
+        body: _widgetOptions.elementAt(_selectedIndex),
+        bottomNavigationBar: BottomAppBar(
+          child: BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            iconSize: 25,
+            showSelectedLabels: true,
+            showUnselectedLabels: true,
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: 'Home',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.search),
+                label: 'Explore',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.list),
+                label: 'Your Trips',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.person),
+                label: 'User',
+              ),
+            ],
+            backgroundColor: Color(0xFF4EAFC1),
+            currentIndex: _selectedIndex,
+            selectedItemColor: Colors.white,
+            onTap: (value) {
+              setState(() {
+                _selectedIndex = value;
+              });
+            },
+          ),
         ),
       ),
     );
